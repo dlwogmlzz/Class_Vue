569b87890c4e97a6ecac914da2b3fc6ff2ede5c3
dlwogmlzz <lwwljh90@gmail.com>
2023-12-10 19:39:26 +0900

    2-lifecycle CountApp

---
+++ 2-lifecycle/CountApp/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <button @click="openApp">OPEN APP</button>
+    <!-- v-if 조건문으로 isOpen이 true일때 CountApp열기 -->
+    <div v-if="isOpen">
+      <CountApp />
+    </div>
+  </div>
+</template>
+
+<script>
+import CountApp from './components/CountApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CountApp
+  },
+  // isOpen으로 초기화
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    openApp() {
+      this.isOpen = !this.isOpen
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 20px;
+}
+</style>
---
+++ 2-lifecycle/CountApp/src/components/CountApp.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<h1>Count: {{ count }}</h1>
+		<!-- @click은 onClick을 의미한다. 함수이름은 {}가 아닌 ""로 넣는다. -->
+		<button @click="increase">+</button>
+		<button @click="decrease">-</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			// useState 개념
+			return {
+				count: 0,
+			}
+		},
+		// count를 처리해줄 함수
+		methods: {
+			increase() {
+				this.count++;
+			},
+			decrease() {
+				this.count--;
+			},
+		},
+		// Life Cycle : beforeCreate, beforeMount, beforeUpdate, beforeDestroy
+  // 처음 컴포넌트가 생성된 시점
+  created() {
+    console.log('Created')
+  },
+  // 처음 컴포넌트가 생성되어 렌더링까지 끝난 시점
+  mounted() {
+    console.log('Mounted')
+  },
+  // 컴포넌트가 업데이트 된 시점
+  updated() {
+    console.log('Updated: ', this.count)
+  },
+  // 컴포넌트가 죽는 시점
+  destroyed() {
+    console.log('Destroyed')
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
